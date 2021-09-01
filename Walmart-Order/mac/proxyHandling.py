9b2536b5eb7d8d49104f806088cd2af90394d4d4
finnbassill <finnbassill@gmail.com>
2021-09-01 19:23:52 -0400

    commit txt

--- Walmart-Order/mac/proxyHandling.py
+++ Walmart-Order/mac/proxyHandling.py
@@ -1,3 +1,21 @@
 
-def proxyHandling(proxies):
-    
+def proxyHandling():
+    ip = []
+    port = []
+    username = []
+    password = []
+
+    with open('proxies.txt') as line:
+        proxy = line.readlines()
+        ip.append(proxy.split(':')[0])
+        port.append(proxy.split(':')[1])
+        username.append(proxy.split(':')[2])
+        password.append(proxy.split(':')[3])
+
+        print(ip)
+        print(port)
+        print(username)
+        print(password)
+
+
+proxyHandling()
