991122d9c8275f7f01ecb59ee32b823ce01eaf63
finnbassill <42978511+finnbassill@users.noreply.github.com>
2021-09-01 18:44:58 -0400

    Add files via upload
    First commit github

---
+++ Walmart-Order/mac/wallyMAC.py
@@ -0,0 +1,92 @@
+from selenium.webdriver.chrome import options
+from seleniumwire import webdriver
+import csv
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+from operator import itemgetter
+import random
+
+    
+#Function that scrapes walmart page to see if product is cancelled or not
+def walmartOrderTracker(rows): 
+
+    #Optional Proxy 
+    #options = {
+    #'proxy': {
+    #    'http': 'http://GPRT413625:AJLOQTV7@216.173.99.17:19270',
+    #    'https': 'http://GPRT413625:AJLOQTV7@216.173.99.17:19270',
+    #    'no_proxy': 'localhost,127.0.0.1'
+    #}}
+    #driver = webdriver.Chrome(r'/Users/finnbassill/Projects/Walmart-Order/mac/chromedriver', seleniumwire_options=options)
+
+    #Webdriver path
+    options = Options()
+    #options.add_argument("--headless")
+    driver = webdriver.Chrome(r'/Users/finnbassill/Projects/Walmart-Order/mac/chromedriver', options=options)
+
+    fields = []
+    updatedRows = []
+    cancels = 0
+    success = 0  
+
+    #Getting website and input box elements
+    for orders in rows:
+        driver.get('https://www.walmart.com/account/trackorder')
+        emailBox = driver.find_element_by_xpath('//input[@name="email" and @id="email"]')
+        numBox = driver.find_element_by_xpath('//input[@name="fullOrderId" and @id="fullOrderId"]')
+
+        #Sending keys with delay email
+        email = orders[1]
+        for x in range(len(orders[1])):
+            emailBox.send_keys(email[x])
+            time.sleep(random.uniform(0.04, 0.08))
+        
+        #Sending keys with delay order num
+        num = orders[3]
+        for x in range(0,13):
+            numBox.send_keys(num[x])
+            time.sleep(random.uniform(0.04, 0.08))
+
+        #Send keys to click enter
+        time.sleep(.1)
+        numBox.send_keys(Keys.TAB)
+        time.sleep(0.13)
+        numBox.send_keys(Keys.ENTER)
+        time.sleep(.7)
+
+        #Checking for captcha solve
+        captchaSolving = True
+        while captchaSolving:
+            try:
+                test = driver.find_element_by_xpath('//div[@id="px-captcha"]')
+                time.sleep(.5)
+            except:
+                captchaSolving = False
+                time.sleep(2)
+            else:
+                print('Caught captcha, waiting to solve...')
+                time.sleep(4)
+
+        #Checking to see if item is cancelled
+        cancelStatus = driver.find_element_by_xpath('//span[@class="shipping-status-text"]').text
+        if cancelStatus == 'Canceled':
+            cancels += 1
+            updatedRows.append([orders[0], orders[1], orders[2], orders[3], 'FASLE'])
+        else:
+            success += 1
+            updatedRows.append([orders[0], orders[1], orders[2], orders[3], 'TRUE'])
+
+        print(str(len(updatedRows)) + '/' + str(len(rows)) + " retrieving walmart order status")
+    
+    driver.quit()
+    return updatedRows, cancels, success
+
+#Creates csv file with all the products
+def csvCreate(newRows, cancels, success):
+    with open('walmartAccounts.csv', 'w', newline='') as file:
+        sortedList = sorted(newRows, key=itemgetter(0))
+        writer = csv.writer(file)
+        writer.writerow(['user', 'email', 'password', 'orderNum', 'success?', '#success= ' + str(success), '#cancels= ' + str(cancels)])
+        writer.writerows(sortedList)
+
---
+++ Walmart-Order/mac/walmartbotMAC.py
@@ -0,0 +1,138 @@
+import discord
+from wallyMAC import *
+from dotenv import load_dotenv
+import datetime
+import os
+
+#Loads the .env file for the api key
+load_dotenv()
+
+#Creates client and guild object
+client = discord.Client()
+server = client.get_guild(698382958133772288)
+
+#Skus for walmart
+PS5DISK = ['994712501', 'Sony PlayStation 5 Video Game Console']
+PS5DIGITAL = ['979738052', 'Sony PlayStation 5, Digital Edition Video Game Consoles']
+XBOXX = ['141335186','Xbox Series X']
+XBOXXHALO = ['288604110', 'Xbox Series X – Halo Infinite Limited Edition Bundle']
+CARDS = ''
+skusStellar = []
+skusKoi = []
+
+#Logging bot in
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+#Class that filters and manages the webhooks in a sepcified channel
+class Client(discord.Client):
+    async def on_ready(self):
+
+        #Asking user for date of webhook
+        userInput = input("Enter date of webhook in format mm/dd/yyyy\n")
+        userInput = userInput.replace('/','')
+        year = userInput[4:]
+        month = userInput[:2]
+        day = userInput[2:4]
+
+        #Asking User to speicify channel
+        webhookChannels = []
+        guild = client.get_guild(698382958133772288)
+        successCategory = discord.utils.get(guild.categories, id=753697996729745499)
+        channels = successCategory.channels
+        for channel in channels:
+            if channel.name != 'success' and channel.name != 'mates-public-log':
+                webhookChannels.append(channel)
+        print('Enter number of the channel where the webhook(s) are.')
+        for number, channel in enumerate(webhookChannels):
+            print(number, channel.name)
+        userInput = int(input(''))
+        channel = webhookChannels[userInput]
+
+        #Asking user to specify skus to test for
+        userInput = input("Check for PS5 Disk? Y/N\n")
+        if userInput == 'Y' or userInput == 'y':
+            skusStellar.append(PS5DISK[0])
+            skusKoi.append(PS5DISK[1])
+        userInput = input("Check for PS5 Digital? Y/N\n")
+        if userInput == 'Y' or userInput == 'y':
+            skusStellar.append(PS5DIGITAL[0])
+            skusKoi.append(PS5DIGITAL[1])
+        userInput = input("Check for Xbox X? Y/N\n")
+        if userInput == 'Y' or userInput == 'y':
+            skusStellar.append(XBOXX[0])
+            skusKoi.append(XBOXX[1])
+        userInput = input("Check for Xbox X - Halo Edition? Y/N\n")
+        if userInput == 'Y' or userInput == 'y':
+            skusStellar.append(XBOXXHALO[0])
+            skusKoi.append(XBOXXHALO[1])
+
+        #Using user date to find day before and after
+        beforeDate = datetime.datetime(int(year), int(month), int(day) + 1, hour=4)
+        afterDate = datetime.datetime(int(year), int(month), int(day) - 1, hour=4)
+
+        #Getting the webhhoks from the specified date
+        messages = await channel.history(limit=500, before=beforeDate, after=afterDate).flatten()
+        koi = 0
+        #Filtering messages and getting fields
+        contentList = []
+        for msg in messages:
+            
+            #Checking if author is StellarAIO
+            if str(msg.author) == 'StellarAIO Checkout#0000':
+
+                #Checking if skus are in embed
+                for s in skusStellar:
+
+                    if f"name='Sku', value='{s}'" in str(msg.embeds[0].fields) and len(msg.embeds[0].fields) == 11:
+
+                        #Getting specific fields and cleaning up string
+                        user = str(msg.embeds[0].fields[7].value.replace('||', ''))
+                        content = []
+                        content.append(user)
+                        email = str(msg.embeds[0].fields[9].value.replace('||', ''))
+                        content.append(email)
+                        password = str(msg.embeds[0].fields[10].value.replace('||', ''))       
+                        content.append(password)
+                        orderNum = str(msg.embeds[0].fields[8].value.replace('||', ''))
+                        content.append(orderNum)
+                        contentList.append(content)
+            
+            #Checking if author is KoiAIO
+            if str(msg.author) != 'StellarAIO Checkout#0000':
+
+                try:
+                    test = msg.embeds[0].fields[3]          
+                except:
+                    pass
+                else:
+                    if str(test.value.replace('||', '')) == 'Auto-Gen' and msg.embeds[0].title == 'Koi AIO | Checkout Successful':
+                       
+                       for s in skusKoi:
+
+                           if str(msg.embeds[0].fields[1].value.replace('||', '')) == s:
+
+                                #Getting specific fields and cleaning up string
+                                user = str(msg.embeds[0].fields[6].value.replace('||', ''))
+                                content = []
+                                content.append(user)
+                                email = str(msg.embeds[0].fields[8].value.replace('||', ''))
+                                content.append(email)
+                                password = str(msg.embeds[0].fields[9].value.replace('||', ''))       
+                                content.append(password)
+                                orderNum = str(msg.embeds[0].fields[10].value.replace('||', ''))
+                                content.append(orderNum)
+                                contentList.append(content)
+        
+        #Calling walmart order checker function
+        checkedOrders = walmartOrderTracker(contentList)
+
+        #Calling spreadsheet creator function
+        csvFile = csvCreate(checkedOrders[0], checkedOrders[1], checkedOrders[2])
+        print('walmartAccount.csv was saved to application folder.')
+        await self.close()
+
+#Running program
+client = Client()
+client.run(os.getenv('TOKEN'))
